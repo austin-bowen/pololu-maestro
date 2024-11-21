cbf42817de1b0e5c8cfcf589e5e5bc99aac148ca
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-20 22:13:16 -0500

    Add __setattr__ unit tests to test_set_target.py

--- test_maestro/test_set_target.py
+++ test_maestro/test_set_target.py
@@ -9,20 +9,43 @@ class TestMaestroSetTarget(BaseMaestroTest):
         (1, 0, b'\x01\x00\x00'),
         (2, 4095.75, b'\x02\x7F\x7F'),
     ])
-    def test_valid_channel_and_target(self, channel: int, target_us: int, suffix: bytes):
+    def test_set_target_with_valid_channel_and_target(self, channel: int, target_us: int, suffix: bytes):
         self.maestro.set_target(channel, target_us)
         self.assert_wrote(b'\xAA\x0C\x04' + suffix)
 
+    @pytest.mark.parametrize('channel, target_us, suffix', [
+        (0, 1500, b'\x00\x70\x2E'),
+        (1, 0, b'\x01\x00\x00'),
+        (2, 4095.75, b'\x02\x7F\x7F'),
+    ])
+    def test_setitem_with_valid_channel_and_target(self, channel: int, target_us: int, suffix: bytes):
+        self.maestro[channel] = target_us
+        self.assert_wrote(b'\xAA\x0C\x04' + suffix)
+
     @pytest.mark.parametrize('channel', [-1, 3])
-    def test_invalid_channel_raises_ValueError(self, channel: int):
+    def test_set_target_with_invalid_channel_raises_ValueError(self, channel: int):
         with pytest.raises(ValueError):
             self.maestro.set_target(channel, 1500)
 
         self.assert_conn_not_used()
 
+    @pytest.mark.parametrize('channel', [-1, 3])
+    def test_setitem_with_invalid_channel_raises_ValueError(self, channel: int):
+        with pytest.raises(ValueError):
+            self.maestro[channel] = 1500
+
+        self.assert_conn_not_used()
+
     @pytest.mark.parametrize('target_us', [-1, 4095.751])
-    def test_invalid_target_raises_ValueError(self, target_us: float):
+    def test_set_target_with_invalid_target_raises_ValueError(self, target_us: float):
         with pytest.raises(ValueError):
             self.maestro.set_target(0, target_us)
+
+        self.assert_conn_not_used()
+
+    @pytest.mark.parametrize('target_us', [-1, 4095.751])
+    def test_setitem_with_invalid_target_raises_ValueError(self, target_us: float):
+        with pytest.raises(ValueError):
+            self.maestro[0] = target_us
 
         self.assert_conn_not_used()
