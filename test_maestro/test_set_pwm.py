a102807464dd3bbaa695feb01699fa70dc0ec6d3
Austin Bowen <austin.bowen.314@gmail.com>
2024-11-22 14:51:28 -0500

    Fix set_pwm unit tests

--- test_maestro/test_set_pwm.py
+++ test_maestro/test_set_pwm.py
@@ -4,24 +4,25 @@ from test_maestro.conftest import BaseMiniMaestroTest
 
 
 class TestMiniMaestroSetPwm(BaseMiniMaestroTest):
-    @pytest.mark.parametrize('on_time_us, period_us, suffix', [
-        (0, 341.3125, b'\x00\x00\x7F\x7F'),
-        (341.3125, 341.3125, b'\x7F\x7F\x7F\x7F'),
-        (50, 100, b'\x60\x12\x40\x25'),
+    @pytest.mark.parametrize('period_us, duty_cycle, suffix', [
+        (341.3125, 0., b'\x00\x00\x7F\x7F'),
+        (341.3125, 1., b'\x7F\x7F\x7F\x7F'),
+        (100, .5, b'\x60\x12\x40\x25'),
     ])
-    def test_valid_on_time_and_period(self, on_time_us: float, period_us: float, suffix: bytes):
+    def test_valid_on_time_and_period(self, period_us: float, duty_cycle: float, suffix: bytes):
         # noinspection PyUnresolvedReferences
-        self.maestro.set_pwm(on_time_us, period_us)
+        self.maestro.set_pwm(period_us, duty_cycle)
         self.assert_wrote(b'\xAA\x0C\x0A' + suffix)
 
-    @pytest.mark.parametrize('on_time_us, period_us', [
-        (-1, 100),
-        (101, 100),
-        (100, 341.3126),
+    @pytest.mark.parametrize('period_us, duty_cycle', [
+        (100, -0.01),
+        (100, 1.01),
+        (-100, .5),
+        (341.3126, .5),
     ])
-    def test_invalid_args_raises_ValueError(self, on_time_us: float, period_us: float):
+    def test_invalid_args_raises_ValueError(self, period_us: float, duty_cycle: float):
         with pytest.raises(ValueError):
             # noinspection PyUnresolvedReferences
-            self.maestro.set_pwm(on_time_us, period_us)
+            self.maestro.set_pwm(period_us, duty_cycle)
 
         self.assert_conn_not_used()
